b571267a3490900017164106257d3be4072c1fdf
Lucky Alade <122607649+luckyalade@users.noreply.github.com>
2023-08-26 00:22:23 +0100

    Delete scripts directory

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-const getUserName = document.querySelector("strong");
-
-getUserName.textContent = prompt("Please enter your first name: ");
-
-let storedUserName = getUserName.textContent;
-
-alert(`Welcome, ${storedUserName}!`);
-alert("Thank you for the input!");
-alert("Happy Reading!");
